de69f4395eb462d93f5a0cd0fb1e13ede58b6a7c
Vinicius Matteus <vmatteus@gmail.com>
2024-09-11 19:59:02 -0300

    test index

---
+++ index.js
@@ -0,0 +1,19 @@
+import { Authenticator } from '@aws-amplify/ui-react';
+import { Amplify } from 'aws-amplify';
+import '@aws-amplify/ui-react/styles.css';
+import outputs from "../amplify_outputs.json";
+
+Amplify.configure(outputs);
+
+export default function App() {
+  return (
+    <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user?.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
+  );
+}
